a4300dc8c404373d538cdb7f6ebfb5b44cc8e8e4
santiago ruiz posso <santiagorupoz@gmail.com>
2024-04-21 15:26:20 -0500

    git init

---
+++ db.js
@@ -0,0 +1,22 @@
+const mysql = require('mysql2');
+require('dotenv').config();
+
+// Configuración de la conexión a la base de datos
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
+  connectTimeout: 60000 //
+});
+
+// Conexión a la base de datos
+connection.connect((err) => {
+  if (err) {
+    console.error('Error de conexión: ', err);
+    return;
+  }
+  console.log('Conexión exitosa a la base de datos MySQL');
+});
+//Solucionar :C
