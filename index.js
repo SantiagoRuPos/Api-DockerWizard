8cd2ae3d96b2af0bbe39c252bd9a9591e0154365
santiago ruiz posso <santiagorupoz@gmail.com>
2024-05-16 18:57:32 -0500

    final 1.0

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const routes = require('./Routes/index');
 
 const listarImagenesDocker = require ('./Controllers/imagenes');
 const app = express();
+
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
